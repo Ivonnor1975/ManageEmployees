3f3b2b2074439c207d464f8bb812d1460f55b62a
Ivon <ivonne@jjf.com>
2021-11-05 09:08:43 -0500

    clean up

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,9 +6,9 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('HR Assistant',10,1),
-  ('Helpdesk',80,2),
-  ('Consultant',10,3);
+  ('HR Assistant',10.15,1),
+  ('Helpdesk',80.20,2),
+  ('Consultant',10.20,3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
